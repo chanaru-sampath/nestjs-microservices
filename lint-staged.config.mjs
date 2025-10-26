e61928c7df86a3279e6066f65ac0cb986a42b418
chanaru-sampath <chanarusampath@gmail.com>
2025-10-26 07:53:14 +0530

    basic setup

---
+++ lint-staged.config.mjs
@@ -0,0 +1,9 @@
+/**
+ * @filename: lint-staged.config.mjs
+ * @type {import('lint-staged').Configuration}
+ */
+const config = {
+  '*.{js,jsx,ts,tsx}': (stagedFiles) => [`prettier --write ${stagedFiles.join(' ')}`],
+};
+
+export default config;
